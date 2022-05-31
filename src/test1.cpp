306dc8c504b529625b3fac19e9f586e6ed137c57
Buhot Antoine <buhot@kth.se>
2022-05-31 18:56:33 +0200

    testing

---
+++ src/test1.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+
+// This is bugprone code (missing parentheses in macro arguments)
+#define sum_bugprone(a,b) (a+b)
+#define multiply_bugprone(a,b) (a*b)
+
+int main(int argc, char *argv[]){
+   std::cout << "Hello World!" << std::endl; //test_change1
+   return 0;
+}
