3a31fcb3f36dd7a03978e7a3573c2aad205e4a54
BerkeYak <104976258+BerkeYak@users.noreply.github.com>
2022-06-02 15:02:22 +0200

    Update clang-tidy-jenkins.sh

--- scripts/clang-tidy-jenkins.sh
+++ scripts/clang-tidy-jenkins.sh
@@ -47,8 +47,8 @@ cat $BUILD_DIR/compile_commands.json \
 
 # Set clang-tidy checks and header-filters
 #CHECKS="-checks=-*,bugprone-*,-bugprone-narrowing-conversions,-bugprone-branch-clone"
-#HEADER_FILTER="-header-filter=*,-*/externals/*"
-CLANG="clang-tidy -p $tidy_dir" # $CHECKS $HEADER_FILTER -p $BUILD_DIR"
+HEADER_FILTER="-header-filter=*,-*/externals/*"
+CLANG="clang-tidy $HEADER_FILTER -p $tidy_dir" # $CHECKS $HEADER_FILTER -p $BUILD_DIR"
 
 # Output file
 CLANG_OUTPUT_FILE="$BUILD_DIR/clang-tidy-output"
