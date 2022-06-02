14be344681a468d95de10e2ec846af61ddcfe48a
BerkeYak <104976258+BerkeYak@users.noreply.github.com>
2022-06-02 15:21:44 +0200

    Update file-without-bugprone-1.cpp

--- src/file-without-bugprone-1.cpp
+++ src/file-without-bugprone-1.cpp
@@ -1,6 +1,6 @@
 #include<iostream>
 
 int main(int argc, char *argv[]){
-   std::cout << "Hello Mauro!" << std::endl; //test_change2
+   std::cout << "Hello Mauro!" << std::endl; //test_change9
    return 0;
 }
